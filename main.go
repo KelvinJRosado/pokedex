6eb0809ecb067bcbd602c320f4419f397aa984ba
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-01 14:47:19 -0400

    chore: Implemented basic REPL

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create scanner instance to read user input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Print standard line
+	fmt.Print("Pokedex > ")
+
+	// Use an infinite loop to keep CLI open
+	for scanner.Scan() {
+		// Get user input
+		input := scanner.Text()
+
+		// Clean user input
+		cleaned := cleanInput(input)
+
+		// Grab 1st word
+		first := cleaned[0]
+
+		// Print message
+		fmt.Printf("Your command was: %v\n", first)
+
+		// Prepare next line
+		fmt.Print("Pokedex > ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Invalid input: %s\n", err)
+	}
 }
