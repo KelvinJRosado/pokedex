fcf414a12f46d17de9f082fef5444122f1ed17f8
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-01 14:58:00 -0400

    refactor: split repl package into multiple files

---
+++ repl/utils.go
@@ -0,0 +1,19 @@
+package repl
+
+import (
+	"strings"
+)
+
+// Split the user's input into words based on whitespace.
+// It should also lowercase the input and trim any leading or trailing whitespace.
+func cleanInput(text string) []string {
+	var out []string
+
+	// First we make everything lowercase
+	lower := strings.ToLower(text)
+
+	// Lastly we split on whitespace, removing leading/trailing whitespace
+	out = strings.Fields(lower)
+
+	return out
+}
