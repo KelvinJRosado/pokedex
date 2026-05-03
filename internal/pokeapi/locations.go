70775b75de2dafd4e09c442d4e8390223d3deadd
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-03 14:37:53 -0400

    feat: add GetLocationAreaDetails function with caching

--- internal/pokeapi/locations.go
+++ internal/pokeapi/locations.go
@@ -56,3 +56,50 @@ func GetLocationAreaSlice(startId, count int, cache *pokecache.Cache) (LocationA
 
 	return locationsData, nil
 }
+
+func GetLocationAreaDetails(name string, cache *pokecache.Cache) (LocationAreaDetails, error) {
+
+	// Build exact path to get location area detail data
+	fullPath := fmt.Sprintf("%vlocation-area/%v", POKEAPI_BASE_URL, name)
+
+	// Store raw data from response
+	var data []byte
+
+	// Check cache
+	cacheRes, hit := cache.Get(fullPath)
+	if hit {
+		data = cacheRes
+		fmt.Println("Serving response from cache")
+	} else {
+		// Built GET request
+		res, err := http.Get(fullPath)
+		if err != nil {
+			fmt.Printf("Error calling LocationArea API: %v\n", err.Error())
+			return LocationAreaDetails{}, err
+		}
+		// Read and parse response
+		data, err = io.ReadAll(res.Body)
+		defer res.Body.Close()
+		if res.StatusCode > 299 {
+			fmt.Printf("LocationArea API call failed with status code: %d and\ndata: %s\n", res.StatusCode, data)
+			return LocationAreaDetails{}, errors.New("LocationArea API call failed")
+		}
+		if err != nil {
+			fmt.Printf("Failed to parse response body: %v\n", err.Error())
+			return LocationAreaDetails{}, errors.New("LocationArea API response could not be parsed")
+		}
+
+		// Save response to cache
+		cache.Add(fullPath, data)
+	}
+
+	// Convert data to struct
+	var locationsData LocationAreaDetails
+	err := json.Unmarshal(data, &locationsData)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal data: %v\n", err.Error())
+		return LocationAreaDetails{}, errors.New("LocationArea API response could not be parsed")
+	}
+
+	return locationsData, nil
+}
--- internal/pokeapi/models.go
+++ internal/pokeapi/models.go
@@ -11,3 +11,18 @@ type LocationAreaList struct {
 	Previous string         `json:"previous"`
 	Results  []LocationArea `json:"results"`
 }
+
+type PokemonSummary struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
+type PokemonEncounter struct {
+	Pokemon PokemonSummary `json:"pokemon"`
+}
+
+type LocationAreaDetails struct {
+	Id         int                `json:"int"`
+	Name       string             `json:"string"`
+	Encounters []PokemonEncounter `json:"pokemon_encounters"`
+}
