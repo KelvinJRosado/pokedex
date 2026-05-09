2623d11aac815fabc78b24b30190e555c47145f3
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-09 16:06:40 -0400

    feat: add generic HTTP fetch utility with caching support

---
+++ internal/pokeapi/utils.go
@@ -0,0 +1,53 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/kelvinjrosado/pokedex/internal/pokecache"
+)
+
+func fetchWithCache[T any](cache *pokecache.Cache, fullPath, label string) (T, error) {
+	// Init respose
+	var response T
+
+	// Store raw data from response
+	var data []byte
+
+	// Check cache
+	cacheRes, hit := cache.Get(fullPath)
+	if hit {
+		data = cacheRes
+		fmt.Println("Serving response from cache") // TODO: Convert to debug log
+	} else {
+		// Built GET request
+		res, err := http.Get(fullPath)
+		if err != nil {
+			return response, err
+		}
+		defer res.Body.Close()
+
+		// Read and parse response
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			return response, err
+		}
+
+		if res.StatusCode > 299 {
+			return response, fmt.Errorf("%v API call failed with status %d: %s", label, res.StatusCode, data)
+		}
+
+		// Save response to cache
+		cache.Add(fullPath, data)
+	}
+
+	// Convert data to struct
+	err := json.Unmarshal(data, &response)
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
